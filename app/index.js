b6637d5233b572b7a4c2c3d0f205ebce2adb69b7
marcos <marcosaureliofarias2023@gmail.com>
2022-03-20 19:49:05 -0300

    fix

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
     res.json({
         version: "1.1.5",
         description: "Container Nginx e NodeJS",
-        maintainer: "Antoniel Lima<antoniel@desenroladev.com.br>"
+        maintainer: "Marcos <marcosaureliofarias2023@gmail.com>"
     });
 });
 
